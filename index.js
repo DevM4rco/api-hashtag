90f68b15e3b769ef1080616306cc469ef4977a2f
DevM4rco <devm4rco@gmail.com>
2024-04-29 19:27:44 -0300

    feat: buscar um time com a sigla

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ app.get('/', (req, res) => {
 	res.send(table);
 });
 
+app.get('/:acronym', (req, res) => {
+	const acronym = req.params.acronym.toUpperCase();
+	const team = table.find(info => info.sigla === acronym);
+	res.send(team);
+});
+
 app.listen(port, () => console.log(`Servidor rodando http://localhost:${port}`));
