40b844e8276e2064a7f6e3fac66707b608f709e5
DevM4rco <devm4rco@gmail.com>
2024-05-01 17:48:39 -0300

    feat: adicionado o tratamento de erro de time não encontrado

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const app = express();
 const port = process.env.PORT || 3300;
 
 app.get('/', (req, res) => {
-	res.send(table);
+	res.status(200).send(table);
 });
 
 app.get('/:acronym', (req, res) => {
 	const acronym = req.params.acronym.toUpperCase();
 	const team = table.find(info => info.sigla === acronym);
-	res.send(team);
+	if (!team) {
+		res
+			.status(404)
+			.send('Não existe na série A do Brasileirão um time com a sigla informada');
+		return;
+	}
+	res.status(200).send(team);
 });
 
 app.listen(port, () => console.log(`Servidor rodando http://localhost:${port}`));
