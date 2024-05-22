c33537d7f81a58c5a7da85ed3c4bf9d11f14b311
DevM4rco <devm4rco@gmail.com>
2024-05-22 20:58:52 -0300

    feat: adicionado validações com Join

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import table from './table.js';
 import express from 'express';
+import table from './table.js';
+import { schema, teamUpdated } from './validate.js';
 
 const app = express();
 app.use(express.json());
@@ -12,19 +13,34 @@ app.get('/', (req, res) => {
 app.get('/:acronym', (req, res) => {
 	const acronym = req.params.acronym.toUpperCase();
 	const team = table.find(info => info.sigla === acronym);
+
 	if (!team) {
 		res
 			.status(404)
 			.send('Não existe na série A do Brasileirão um time com a sigla informada');
 		return;
 	}
+
 	res.status(200).send(team);
 });
 
 app.put('/:acronym', (req, res) => {
 	const acronym = req.params.acronym.toUpperCase();
 	const teamSelected = table.find(team => team.sigla === acronym);
+	const { error } = teamUpdated.validate(req.body);
 	const tableKeys = Object.keys(req.body);
+
+	if (!teamSelected) {
+		res
+			.status(404)
+			.send('Não existe na série A do Brasileirão um time com a sigla informada');
+		return;
+	}
+
+	if (error) {
+		res.status(400).send(error);
+	}
+
 	tableKeys.forEach(key => (teamSelected[key] = req.body[key]));
 
 	res.status(200).send(teamSelected);
@@ -32,15 +48,29 @@ app.put('/:acronym', (req, res) => {
 
 app.post('/', (req, res) => {
 	const newTeam = req.body;
+	const { error } = schema.validate(newTeam);
+
+	if (error) {
+		res.status(400).send(error);
+		return;
+	}
+
 	table.push(newTeam);
-	res.status(200).send(newTeam);
+	res.status(201).send(newTeam);
 });
 
 app.delete('/:acronym', (req, res) => {
 	const acronym = req.params.acronym.toUpperCase();
 	const indexSelected = table.findIndex(team => team.sigla === acronym);
-
 	const teamDeleted = table.splice(indexSelected, 1);
+
+	if (indexSelected === -1) {
+		res
+			.status(404)
+			.send('Não existe na série A do Brasileirão um time com a sigla informada');
+		return;
+	}
+
 	res.status(200).send(teamDeleted);
 });
 
